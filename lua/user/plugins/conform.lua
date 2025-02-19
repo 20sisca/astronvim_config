52481bd8f7bf94cd363c1a3ffbce03dc1962dc39
alexis.sisca <alexis.sisca@bsport.io>
2025-02-19 14:14:48 +0100

    fix(conform): seems a bit better

--- lua/user/plugins/conform.lua
+++ lua/user/plugins/conform.lua
@@ -3,28 +3,42 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      python = { "ruff_organize_imports" },
+      -- python = { "ruff_organize_imports" },
+      python = function(bufnr)
+        if require("conform").get_formatter_info("ruff_format", bufnr).available then
+          return { "ruff_format", "ruff_organize_imports", "ruff_fix" }
+        else
+          return { "isort", "black" }
+        end
+      end,
     },
+    notify_no_formatters = true,
     formatters = {
-      ruff_organize_imports = {
-        command = "pipenv run ruff",
-        args = {
-          "check",
-          "--force-exclude",
-          "--select=I001",
-          "--fix",
-          "--exit-zero",
-          "--stdin-filename",
-          "$FILENAME",
-          "-",
-        },
-        stdin = true,
-        cwd = util.root_file { -- Ensure `util` is properly required
-          "pyproject.toml",
-          "ruff.toml",
-          ".ruff.toml",
-        } or vim.fn.getcwd(), -- Fallback to current working directory
-      },
+      -- ruff_organize_imports = {
+      --   command = "/home/alexis/.local/bin/ruff run ruff",
+      --   args = {
+      --     "check",
+      --     -- "--force-exclude",
+      --     -- "--select=I001",
+      --     -- "--fix",
+      --     -- "--exit-zero",
+      --     -- "--stdin-filename",
+      --     -- "$FILENAME",
+      --     -- "-",
+      --   },
+      --   stdin = true,
+      --   cwd = util.root_file { -- Ensure `util` is properly required
+      --     "pyproject.toml",
+      --     "ruff.toml",
+      --     ".ruff.toml",
+      --   },
+      --   require_cwd = true,
+      -- },
+    },
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = "fallback",
     },
   },
 }
